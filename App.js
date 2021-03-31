8a4db52a814f6f30ae459130933d7afa91e6ac4b
Baochuan Lu <lubaochuan@gmail.com>
2021-03-31 06:45:28 -0500

    add stack navigotor in a tab

--- App.js
+++ App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Button, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Details!</Text>
+      </View>
+    );
+  }
+}
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        {/* other code from before here */}
         <Button
-          title="Go to Settings"
-          onPress={() => this.props.navigation.navigate('Settings')}
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
     );
@@ -22,40 +33,46 @@ class SettingsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
+        {/* other code from before here */}
         <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
     );
   }
 }
 
-const TabNavigator = createBottomTabNavigator(
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen,
+});
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+  Details: DetailsScreen,
+});
+
+
+export default createAppContainer(createBottomTabNavigator(
   {
-    Home: HomeScreen,
-    Settings: SettingsScreen,
+    Home: { screen: HomeStack },
+    Settings: { screen: SettingsStack },
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+      tabBarIcon: ({ focused, tintColor }) => {
         const { routeName } = navigation.state;
-        let IconComponent = Ionicons;
         let iconName;
         if (routeName === 'Home') {
-          iconName = focused
-            ? 'ios-information-circle'
-            : 'ios-information-circle-outline';
-          // Sometimes we want to add badges to some icons.
-          // You can check the implementation below.
-          IconComponent = HomeIconWithBadge;
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
         } else if (routeName === 'Settings') {
-          iconName = focused ? 'ios-list-box' : 'ios-list';
+          iconName = `ios-options${focused ? '' : '-outline'}`;
         }
 
-        // You can return any component that you like here!
-        return <IconComponent name={iconName} size={25} color={tintColor} />;
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
       },
     }),
     tabBarOptions: {
@@ -63,43 +80,4 @@ const TabNavigator = createBottomTabNavigator(
       inactiveTintColor: 'gray',
     },
   }
-);
-
-class IconWithBadge extends React.Component {
-  render() {
-    const { name, badgeCount, color, size } = this.props;
-    return (
-      <View style={{ width: 24, height: 24, margin: 5 }}>
-        <Ionicons name={name} size={size} color={color} />
-        {badgeCount > 0 && (
-          <View
-            style={{
-              // If you're using react-native < 0.57 overflow outside of parent
-              // will not work on Android, see https://git.io/fhLJ8
-              position: 'absolute',
-              right: -6,
-              top: -3,
-              backgroundColor: 'red',
-              borderRadius: 6,
-              width: 12,
-              height: 12,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
-              {badgeCount}
-            </Text>
-          </View>
-        )}
-      </View>
-    );
-  }
-}
-
-const HomeIconWithBadge = props => {
-  // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
-  return <IconWithBadge {...props} badgeCount={3} />;
-};
-
-export default createAppContainer(TabNavigator);
+));
